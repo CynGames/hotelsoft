550a51234f08cf43412ac3cc492e88da5fb0f35b
CynGames <tomasm.leguizamon@gmail.com>
2023-09-04 18:35:48 -0300

    Reservation entity and prisma schema fixes

--- src/reservations/entities/reservation.entity.ts
+++ src/reservations/entities/reservation.entity.ts
@@ -1,7 +1,38 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
 
 @ObjectType()
 export class Reservation {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  @IsNotEmpty()
+  @IsUUID()
+  reservationID: string;
+
+  @Field(() => ID)
+  @IsUUID()
+  @IsOptional()
+  guestID: string;
+
+  @Field(() => ID)
+  @IsUUID()
+  @IsOptional()
+  roomID: string;
+
+  @Field(() => Date)
+  @IsNotEmpty()
+  checkInDate: Date;
+
+  @Field(() => Date)
+  @IsNotEmpty()
+  checkOutDate: Date;
+
+  @Field(() => String)
+  @IsNotEmpty()
+  status: ReservationStatus;
+}
+
+enum ReservationStatus {
+  PENDING = 'PENDING',
+  CONFIRMED = 'CONFIRMED',
+  CANCELLED = 'CANCELLED',
 }
